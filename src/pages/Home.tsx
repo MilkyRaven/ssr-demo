fb40cb93603b686c894e01e7a61a626852aeb51a
Milky Raven <heartonllamas@gmail.com>
2025-08-26 15:08:34 +0200

    feat: add favorites

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,36 @@
-import { Link, useLoaderData } from "react-router";
+import { useLoaderData } from "react-router";
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
+
+    const [favorites, setFavorites] = useState<any[]>([])
+
+    const toggleFavorite = (movie: any) => {
+        setFavorites((prev) => prev.includes(movie)
+            ? prev.filter(favId => favId !== movie)
+            : [...prev, movie])
+    }
 
     const movies = useLoaderData() as { id: number; title: string }[];
     return (
         <div>
             <h1>Movies</h1>
-            <ul>
-                {movies.map((m) => (
-                    <li key={m.id}>
-                        <p>{m.title}</p>
-                        <Link to={`/movie/${m.id}`} reloadDocument>Ver más</Link>
-                        <a href={`/movie/${m.id}`}>{m.title}</a>
-                    </li>
+            <ul style={{ display: "flex", flexDirection: "row" }}>
+                {movies.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie} toggleFavorite={toggleFavorite} />
                 ))}
             </ul>
+            {/* Favorites */}
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <p>Listado de favoritos</p>
+                {favorites.map((favorite) => (
+                    <div key={favorite.id}>
+                        {favorite.id}
+                        <MovieCard key={favorite} movie={favorite} toggleFavorite={toggleFavorite} />
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
